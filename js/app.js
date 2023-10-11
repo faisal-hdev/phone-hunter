a3cc2f1b71ca70f0635a8ce63aca6b634f9138b2
faisal-hdev <itsfaisalchowdhury@gmail.com>
2023-10-11 17:52:51 +0600

    Phones No phone found validation and spinner loading feature are added

--- js/app.js
+++ js/app.js
@@ -8,15 +8,26 @@ const loadPhones = async (searchText) => {
 const displayPhones = (phones) => {
   const phonesContainer = document.getElementById("phones-container");
   phonesContainer.innerHTML = "";
+  // Display 20 phones only
   phones = phones.slice(0, 20);
+
+  // Display no phones found
+  const noPhone = document.getElementById("no-found-msg");
+  if (phones.length === 0) {
+    noPhone.classList.remove("d-none");
+  } else {
+    noPhone.classList.add("d-none");
+  }
+
+  // Display all phones
   phones.forEach((phone) => {
     const phoneDiv = document.createElement("col");
     phoneDiv.classList.add("col");
     phoneDiv.innerHTML = `
-        <div class="card h-100 p-4">
-        <img src="${phone.image}" class="card-img-top" alt="..." />
+        <div class="card h-100 ">
+        <img src="${phone.image}" class="card-img-top p-4" alt="..." />
         <div class="card-body">
-            <h5 class="card-title">Phone Name ${phone.phone_name}</h5>
+            <h5 class="card-title">Phone Name : ${phone.phone_name}</h5>
             <p class="card-text">
             This is a longer card with supporting text below as a natural
             lead-in to additio nal content. This content is a little bit
@@ -27,12 +38,26 @@ const displayPhones = (phones) => {
       `;
     phonesContainer.appendChild(phoneDiv);
   });
+  // stop loader
+  toggleSpinner(false);
 };
 
+// Handle search button clicked
 document.getElementById("btn-search").addEventListener("click", function () {
+  // start loader
+  toggleSpinner(true);
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
   loadPhones(searchText);
 });
 
-loadPhones();
+const toggleSpinner = (isLoading) => {
+  const loaderElement = document.getElementById("loader");
+  if (isLoading) {
+    loaderElement.classList.remove("d-none");
+  } else {
+    loaderElement.classList.add("d-none");
+  }
+};
+
+// loadPhones();
